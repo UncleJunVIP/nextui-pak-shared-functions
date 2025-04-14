65d4815b71eaa592bdb7b07c5383920dc5186b66
Brandon T. Kowalski <brandon@kowalski.io>
2025-04-14 00:23:21 -0400

    Update model item

--- models/item.go
+++ models/item.go
@@ -4,18 +4,29 @@ import "go.uber.org/zap/zapcore"
 
 type Item struct {
 	DisplayName string `json:"name"`
-	Tag         string `json:"tag"`
-	Filename    string `json:"filename"`
-	Path        string `json:"path"`
-	IsDirectory bool   `json:"is_directory"`
+
+	Filename     string `json:"filename"`
+	Path         string `json:"path"`
+	IsDirectory  bool   `json:"is_directory"`
+	FileSize     string `json:"file_size"`
+	LastModified string `json:"last_modified"`
+
+	Tag string `json:"tag"`
+
+	RomID  string `json:"-"` // For RomM Support
+	ArtURL string `json:"-"` // For RomM Support
 }
 
 func (i Item) MarshalLogObject(encoder zapcore.ObjectEncoder) error {
 	encoder.AddString("name", i.DisplayName)
-	encoder.AddString("tag", i.Tag)
 	encoder.AddString("filename", i.Filename)
 	encoder.AddString("path", i.Path)
 	encoder.AddBool("is_directory", i.IsDirectory)
+	encoder.AddString("file_size", i.FileSize)
+	encoder.AddString("last_modified", i.LastModified)
+	encoder.AddString("tag", i.Tag)
+	encoder.AddString("rom_id", i.RomID)
+	encoder.AddString("art_url", i.ArtURL)
 
 	return nil
 }
