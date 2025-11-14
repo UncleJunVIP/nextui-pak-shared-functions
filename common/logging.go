26e145c0f41f9c1c8b65857c90b22e2a5b62ea9a
Brandon T. Kowalski <brandon@kowalski.io>
2025-11-13 22:47:00 -0500

    Tie into gaba logging.

--- common/logging.go
+++ common/logging.go
@@ -1,24 +1,12 @@
 package common
 
 import (
-	"io"
 	"log"
 	"log/slog"
 	"os"
-	"path/filepath"
-	"strings"
-	"sync"
 
-	"go.uber.org/atomic"
+	gaba "github.com/UncleJunVIP/gabagool/pkg/gabagool"
 )
-
-var logFile *os.File
-var logger atomic.Pointer[slog.Logger]
-var currentLevel atomic.Pointer[slog.LevelVar]
-
-var LoggerInitialized atomic.Bool
-
-var onceLogger sync.Once
 
 func LogStandardFatal(msg string, err error) {
 	log.SetOutput(os.Stderr)
@@ -26,75 +14,5 @@ func LogStandardFatal(msg string, err error) {
 }
 
 func GetLoggerInstance() *slog.Logger {
-	onceLogger.Do(func() {
-		logger.Store(createLogger())
-	})
-	return logger.Load()
-}
-
-func CloseLogger() {
-	if logFile != nil {
-		logFile.Close()
-	}
-}
-
-func createLogger() *slog.Logger {
-	LoggerInitialized.Store(false)
-
-	exePath, err := os.Executable()
-	if err != nil {
-		LogStandardFatal("Couldn't determine executable path: %v", err)
-	}
-
-	logFileName := filepath.Base(exePath) + ".log"
-
-	cwd, err := os.Getwd()
-	if err != nil {
-		LogStandardFatal("Failed to get current working directory", err)
-	}
-
-	logFile, err = os.OpenFile(filepath.Join(cwd, logFileName), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		LogStandardFatal("Unable to open log file!", err)
-	}
-
-	// Create a level variable for dynamic level changes
-	levelVar := &slog.LevelVar{}
-	levelVar.Set(slog.LevelInfo) // Default level
-	currentLevel.Store(levelVar)
-
-	// Create a multi-writer to write to both console and file
-	multiWriter := io.MultiWriter(os.Stdout, logFile)
-
-	// Create a clean JSON handler
-	handler := slog.NewJSONHandler(multiWriter, &slog.HandlerOptions{
-		Level:     levelVar,
-		AddSource: false,
-	})
-
-	LoggerInitialized.Store(true)
-
-	return slog.New(handler)
-}
-
-func SetLogLevel(rawLevel string) {
-	var level slog.Level
-
-	switch strings.ToLower(rawLevel) {
-	case "debug":
-		level = slog.LevelDebug
-	case "info":
-		level = slog.LevelInfo
-	case "warn", "warning":
-		level = slog.LevelWarn
-	case "error":
-		level = slog.LevelError
-	default:
-		level = slog.LevelInfo
-	}
-
-	// Update the level variable to change the logging level dynamically
-	if levelVar := currentLevel.Load(); levelVar != nil {
-		levelVar.Set(level)
-	}
+	return gaba.GetLoggerInstance()
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/UncleJunVIP/nextui-pak-shared-functions
 go 1.24
 
 require (
+	github.com/UncleJunVIP/gabagool v0.1.5
 	github.com/activcoding/HTML-Table-to-JSON v0.0.4
-	go.uber.org/atomic v1.11.0
 	go.uber.org/zap v1.27.0
 	qlova.tech v0.1.1
 )
@@ -12,6 +12,10 @@ require (
 require (
 	github.com/PuerkitoBio/goquery v1.10.3 // indirect
 	github.com/andybalholm/cascadia v1.3.3 // indirect
+	github.com/holoplot/go-evdev v0.0.0-20250804134636-ab1d56a1fe83 // indirect
+	github.com/patrickhuber/go-types v0.6.2 // indirect
+	github.com/veandco/go-sdl2 v0.4.40 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/net v0.39.0 // indirect
 )
